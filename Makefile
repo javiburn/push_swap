10da89c71ea66dbffaa61de45ec73b52e63aeb77
javiburn <jsarabia98@gmail.com>
2023-04-24 15:08:33 +0200

    starting new repository

---
+++ Makefile
@@ -0,0 +1,55 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/04/10 15:00:46 by jsarabia          #+#    #+#              #
+#    Updated: 2023/04/20 17:37:49 by jsarabia         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = push_swap
+
+CC = gcc
+
+LIBFT = libft
+PRINTF = ft_printf
+
+LIBFTOUT = libft/libft.a
+PRINTFOUT = ft_printf/libftprintf.a
+
+OBJS = push_swap.o stack_init.o check.o print_error.o print_list.o movements.o check_if_sorted.o instructions_one.o instructions_two.o instructions_three.o utils.o movements_b.o
+#check.o print_list.o
+#movements.o more_movements.o movements_even_more.o
+
+CFLAGS = -Wall -Werror -Wextra
+
+all: $(NAME)
+
+$(NAME): compile_libft compile_printf $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) $(LIBFTOUT) $(PRINTFOUT) -o $(NAME)
+
+compile_libft:
+	@make bonus -C libft/
+
+compile_printf:
+	@make all -C ft_printf/
+
+%.o : %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	@make clean -C libft/
+	@make clean -C ft_printf/
+	/bin/rm -f *.o
+
+fclean: clean
+	@make fclean -C libft/
+	@make fclean -C ft_printf/
+	rm -rf push_swap
+
+re: fclean all
+
+.PHONY: clean fclean re
---
+++ inc/push_swap.h
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/11 11:47:27 by jsarabia          #+#    #+#             */
+/*   Updated: 2023/04/20 17:56:00 by jsarabia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "ft_printf.h"
+# include "libft.h"
+
+typedef struct s_chunk
+{
+	struct s_list	*stack_a;
+	struct s_list	*stack_b;
+	int				average;
+	int				optimus;
+	int				opt_top;
+	int				pos_opt_top;
+	int				opt_bot;
+	int				pos_opt_bot;
+	int				argcs;
+	int				*num_arr;
+	int				*arr_ordered;
+}					t_chunk;
+
+char	**create_values(int argc, char **argv);
+int		check(char **values, int argc);
+int		*check_int(char **values, int argc);
+t_chunk	order_nums(int *nums, int argc, t_chunk chunk);
+t_chunk	stack_init(t_chunk chunk);
+int		print_arg_error(char *str);
+int		print_int_error(char *str, char **values);
+int		print_list(t_chunk chunk);
+t_chunk	movements(t_chunk chunk, int argc);
+t_chunk	movements_b(t_chunk chunk);
+int		is_sorted(t_chunk chunk);
+int		min_num(t_chunk chunk);
+int		stack_a_sorted(t_chunk chunk);
+int		stack_b_sorted(t_chunk chunk);
+t_list	*ptr_prev_last(t_list *ptr);
+int		array_num(t_chunk chunk);
+t_chunk	pa( t_chunk chunk);
+t_chunk	pb(t_chunk chunk);
+t_chunk	ss(t_list *stack_a, t_list *stack_b, t_chunk chunk);
+t_chunk	rr(t_chunk chunk);
+t_chunk	rrr(t_chunk chunk);
+t_chunk	sa(t_chunk chunk);
+t_chunk	ra(t_chunk chunk);
+t_chunk	rra(t_chunk chunk);
+t_chunk	sb(t_chunk chunk);
+t_chunk	rb(t_chunk chunk);
+t_chunk	rrb(t_chunk chunk);
+t_chunk	sort_b(t_chunk chunk);
+
+//int		check_nums(char **values, int argc);
+/*
+int		check(int argc, char **argv, int rest);
+t_list	*stack_init(int argc, char **argv);
+int		is_sorted(t_list *stack_a, t_list *stack_b);
+int		stack_b_sorted(t_list *stack_b);
+int		stack_a_sorted(t_list *stack_a);
+int		print_list(t_list *stack_a, t_list *stack_b);
+t_chunk	movements(t_chunk chunk);
+t_chunk	movements_more(t_chunk chunk);
+t_chunk	movements_even_more(t_chunk chunk);
+t_list	*sa(t_list *stack_a);
+t_chunk	ss(t_list *stack_a, t_list *stack_b, t_chunk chunk);
+t_list	*pb(t_list *stack_a, t_list *stack_b);
+t_chunk	pa( t_chunk chunk);
+t_list	*ra(t_list *stack_a);
+t_list	*rb(t_list *stack_b);
+t_chunk	rra(t_chunk chunk);
+t_list	*sb(t_list *stack_b);
+t_chunk	rr(t_chunk chunk);
+t_chunk	rrb(t_chunk chunk);
+int		max_value(t_chunk chunk);
+*/
+
+#endif
