4a345b5e10caa7438870cbb0a7af79bec110157c
javiburn <jsarabia98@gmail.com>
2023-04-25 18:43:19 +0200

    WIT input of 6 argcs

--- algorithm.c
+++ algorithm.c
@@ -6,23 +6,31 @@
 /*   By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 15:56:05 by jsarabia          #+#    #+#             */
-/*   Updated: 2023/04/25 18:08:43 by jsarabia         ###   ########.fr       */
+/*   Updated: 2023/04/25 18:42:25 by jsarabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/push_swap.h"
 
-t_chunk	movements_five(t_chunk chunk)
+t_chunk	movements_six(t_chunk chunk)
 {
-	t_list	*prev;
+	int	i;
+	int	n;
 
-	prev = ptr_prev_last(chunk.stack_a);
+	i = chunk.argcs / 2;
+	n = 0;
+	//while ()
+	return (chunk);
+}
+
+t_chunk	movements_three(t_chunk chunk)
+{
 	while (!is_sorted(chunk))
 	{
 		if (ft_atoi(chunk.stack_a->content)
 			> ft_atoi(chunk.stack_a->next->content))
 			return (sa(chunk));
-		if (ft_atoi(prev->content)
+		if (ft_atoi(ptr_prev_last(chunk.stack_a)->content)
 			> ft_atoi(ft_lstlast(chunk.stack_a)->content)
 			|| ft_atoi(chunk.stack_a->content)
 			< ft_atoi(ft_lstlast(chunk.stack_a)->content))
@@ -39,7 +47,13 @@ t_chunk	algorithm(t_chunk chunk)
 	if (chunk.argcs <= 3)
 	{
 		while (!is_sorted(chunk))
-			chunk = movements_five(chunk);
+			chunk = movements_three(chunk);
+		return (chunk);
+	}
+	if (chunk.argcs <= 6)
+	{
+		while (!is_sorted(chunk))
+			chunk = movements_six(chunk);
 		return (chunk);
 	}
 		 //vamos a pasar un número que va a ser i, que será el que determine el valor del size que vamos a pasar al stack_b
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 15:05:58 by jsarabia          #+#    #+#             */
-/*   Updated: 2023/04/25 17:09:46 by jsarabia         ###   ########.fr       */
+/*   Updated: 2023/04/25 18:30:04 by jsarabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int	main(int argc, char **argv)
 	t_chunk	chunk;
 	char	**values;
 
-	//atexit(leaks);
+	atexit(leaks);
 	values = NULL;
 	if (argc < 2)
 		return (print_arg_error
