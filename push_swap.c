05eb237bdfa599dc91b29fb2b4b475a2d85468d3
javiburn <jsarabia98@gmail.com>
2023-05-04 12:47:43 +0200

    hola

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 15:05:58 by jsarabia          #+#    #+#             */
-/*   Updated: 2023/05/04 12:15:45 by jsarabia         ###   ########.fr       */
+/*   Updated: 2023/05/04 12:47:25 by jsarabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int	main(int argc, char **argv)
 	t_chunk	chunk;
 	char	**values;
 
-	//atexit(leaks);
+	atexit(leaks);
 	values = NULL;
 	if (argc < 2)
 		return (print_arg_error
@@ -47,6 +47,7 @@ int	main(int argc, char **argv)
 	chunk = order_nums(chunk);
 	chunk = stack_init(chunk);
 	chunk = algorithm(chunk);
+	free_matrix(values);
 	//print_list(chunk);
 	exit(0);
 	return (0);
